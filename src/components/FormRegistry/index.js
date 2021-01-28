c874283823b4d97c5eead9e3173fe8f9023633d8
devpolatto <polatto.xcode.job@gmail.com>
2021-01-28 20:22:38 -0300

    push to github

--- src/components/FormRegistry/index.js
+++ src/components/FormRegistry/index.js
@@ -12,8 +12,8 @@ function FormRegistry() {
   const [ startDate , setStartDate ] = useState('');
   const [ team , setTeam ] = useState('');
 
-  function handleNewUser(e) {
-    e.preventDefault()
+  async function handleNewUser(e) {
+    
     const data = { name, email, cpf, birthDate, gender, startDate, team }
 
     return console.log(data)
@@ -32,11 +32,11 @@ function FormRegistry() {
 
           <div className="input-group">
             <div className="date">
-              <label for="date">Your Birth Date</label>
+              <label>Your Birth Date</label>
               <input type="date" value={birthDate} placeholder="Birth Date *" onChange={e => setBirthDate(e.target.value)} />
             </div>
             <div className="gender">
-              <label for="gender">Your Gender</label>
+              <label>Your Gender</label>
               <select name="gender" value={gender} onChange={e => setGender(e.target.value)}>
                 <option value="masculine">Masculine</option>
                 <option value="feminine">Feminine</option>
@@ -44,7 +44,7 @@ function FormRegistry() {
               </select>
             </div>
             <div className="team">
-              <label for="team">Team</label>
+              <label>Team</label>
               <select name="team" value={team} onChange={e => setTeam(e.target.value)}>
                 <option value="front-end">Front-End</option>
                 <option value="back-end">Back-End</option>
@@ -54,7 +54,7 @@ function FormRegistry() {
           </div>
 
           <div className="startDate">
-            <label for="startDate">Start Date</label>
+            <label>Start Date</label>
             <input type="date" value={startDate} onChange={e => setStartDate(e.target.value)} />
           </div>
 
